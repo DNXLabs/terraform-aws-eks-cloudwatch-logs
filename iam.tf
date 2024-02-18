e2b56e747bf22524b47073294e953bb0e8129919
Adi <7adityaraj@gmail.com>
2024-02-18 17:27:19 +0200

    Add and operator for iam role attachment

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role_policy_attachment" "cloudwatch_logs" {
-  count      = var.enabled ? 1 : 0
+  count      = var.enabled && var.iam_policy_attach ? 1 : 0
   role       = var.worker_iam_role_name
   policy_arn = data.aws_iam_policy.cloudwatch_logs.arn
 }
